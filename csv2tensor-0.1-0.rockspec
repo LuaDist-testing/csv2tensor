953396e7577c3110262b35676373bd66cc7d10ed
LunaCI <lunaci@luadist.org>
2018-06-08 00:02:26 +0000

    Update to version 0.1-0

---
+++ csv2tensor-0.1-0.rockspec
@@ -0,0 +1,37 @@
+-- This file was automatically generated for the LuaDist project.
+
+package = "csv2tensor"
+version = "0.1-0"
+
+-- LuaDist source
+source = {
+  tag = "0.1-0",
+  url = "git://github.com/LuaDist-testing/csv2tensor.git"
+}
+-- Original source
+-- source = {
+--     url = "git://github.com/willkurt/csv2tensor.git",
+--     tag = "v0.1"
+-- }
+
+description = {
+    summary = "Load CSV files into Torch7 Tensors",
+    detailed = [[
+Provides a simple function to convert a CSV file  of float/ints into a torch.Tensor. Also allows for including or excluding specific columns in the CSV by name (particularly useful when one column contains labels, or has unnessecary columns
+]],
+    homepage = "https://github.com/willkurt/csv2tensor",
+    license = "MIT"
+}
+
+dependencies = {
+    "torch >= 7.0",
+    "csvigo >= 1.0-0"
+}
+
+build = {
+    type = "builtin",
+    modules = {
+       ['init'] = 'init.lua',
+       ['csv2tensor'] = 'csv2tensor.lua' 
+    }
+}
---
+++ csv2tensor.lua
@@ -0,0 +1,75 @@
+require 'torch'
+require 'csvigo'
+
+local csv2tensor = {}
+
+local function all_cols(csv_data)
+   local col_names = {}
+   local ncols = 0
+   for k,v in pairs(csv_data) do
+      ncols = ncols + 1
+      col_names[ncols] = k
+   end
+   return col_names, ncols
+end
+
+local function _in(s,ls)
+   for _, v in ipairs(ls) do
+      if v == s then
+         return true
+      end
+   end
+   return false
+end
+
+local function exclude_cols(csv_data,ignore)
+   local col_names = {}
+   local ncols = 0
+   for k,v in pairs(csv_data) do
+      if not  _in(k,ignore) then
+         ncols = ncols + 1
+         col_names[ncols] = k
+      end
+   end
+   return col_names, ncols
+end
+
+local function include_cols(csv_data,keep)
+   local col_names = {}
+   local ncols = 0
+   for k,v in pairs(csv_data) do
+      if _in(k,keep) then
+         ncols = ncols + 1
+         col_names[ncols] = k
+      end
+   end
+   return col_names, ncols
+end
+
+
+function csv2tensor.load (path,opts)
+   print("loading data from csv")
+   if(opts and opts.exclude and opts.include) then
+      print("Warning: both 'exclude' and 'include' specified. Using 'exclude' only")
+   end
+   local csv_data  = csvigo.load{path=path}
+   local col_names, ncols
+   if opts and opts.exclude then
+      col_names, ncols = exclude_cols(csv_data,opts.exclude)
+   elseif opts and opts.include then
+      col_names, ncols = include_cols(csv_data,opts.include)
+   else
+      col_names, ncols = all_cols(csv_data)
+   end
+   local n_rows = #csv_data[col_names[1]]
+   print("creating tensor")
+   local tensor_data = torch.Tensor(n_rows,#col_names)
+   for i,col_name in ipairs(col_names) do
+      next_col =  torch.Tensor(csv_data[col_name])
+      tensor_data[{{},i}] = next_col
+   end
+   return tensor_data, col_names
+end
+
+
+return csv2tensor
